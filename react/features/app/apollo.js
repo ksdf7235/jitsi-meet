1295336d632fd68b3a36e59f562a936e4b16d59b
ksdf7235 <85724877+ksdf7235@users.noreply.github.com>
2022-06-07 17:52:44 +0900

    update(apollo)

--- react/features/app/apollo.js
+++ react/features/app/apollo.js
@@ -3,6 +3,6 @@ import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
 export const isLoggedInVar = makeVar(false);
 export const darkModeVar = makeVar(false);
 export const client = new ApolloClient({
-    uri: "http://34.64.222.135:4000/graphql",
+    uri: "https://34.64.222.135:4000/graphql",
     cache: new InMemoryCache(),
 });
