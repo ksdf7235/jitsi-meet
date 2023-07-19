13c6ed1d0ca9c82488c045bd38891e8639f76178
ksdf7235 <85724877+ksdf7235@users.noreply.github.com>
2023-07-19 11:15:08 +0900

    update(disable nxdf)

--- react/nxdf/components/component/apollo.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
-
-export const isLoggedInVar = makeVar(false);
-export const darkModeVar = makeVar(false);
-export const client = new ApolloClient({
-    uri: "http://localhost:4000/graphql",
-    cache: new InMemoryCache(),
-});
