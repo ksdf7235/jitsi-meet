814db54de595eebd95801432cce706b9448d2898
ksdf7235 <85724877+ksdf7235@users.noreply.github.com>
2023-07-21 14:36:01 +0900

    feate(backbtn, toolbar height)

--- react/seongju/backbtn.js
+++ react/seongju/backbtn.js
@@ -1,35 +1,30 @@
-import { useMutation } from "@apollo/client";
-import dayjs from "dayjs";
 import React, { useState } from "react";
 
-import { dummy } from "./dummy";
-import { EXIT_QUERY, LOG_EXIT_QUERY } from "./mutation";
-
-function Backbtn(value) {
-    const dbNow = dayjs().add(9, "hour");
-    const [EnterTime, setEnterTime] = useState(dbNow);
-    const { name, location } = value;
-    const hostNameEng = location.substr(1);
-    const hostNameKr = dummy
-        .filter((a) => a.roomNameEng === hostNameEng)
-        .map((data) => data.roomNamekr)[0];
-    const [backdata] = useMutation(EXIT_QUERY);
-    const [logbackdata] = useMutation(LOG_EXIT_QUERY);
+function Backbtn() {
+    // const dbNow = dayjs().add(9, "hour");
+    // const [EnterTime, setEnterTime] = useState(dbNow);
+    // const { name, location } = value;
+    // const hostNameEng = location.substr(1);
+    // const hostNameKr = dummy
+    //     .filter((a) => a.roomNameEng === hostNameEng)
+    //     .map((data) => data.roomNamekr)[0];
+    // const [backdata] = useMutation(EXIT_QUERY);
+    // const [logbackdata] = useMutation(LOG_EXIT_QUERY);
 
     const back = () => {
-        logbackdata({
-            variables: {
-                participantNameKr: name,
-                hostNameEng,
-                hostNameKr,
-                enteredDate: EnterTime,
-            },
-        });
-        backdata({
-            variables: {
-                participantNameKr: name,
-            },
-        });
+        // logbackdata({
+        //     variables: {
+        //         participantNameKr: name,
+        //         hostNameEng,
+        //         hostNameKr,
+        //         enteredDate: EnterTime,
+        //     },
+        // });
+        // backdata({
+        //     variables: {
+        //         participantNameKr: name,
+        //     },
+        // });
 
         history.back();
     };
